f798fe2ef18c7ffb80cded8def5d9fa013de3467
axell-brendow <breno.axel@gmail.com>
2020-07-22 14:08:11 -0300

    Increase timeout for db and app

--- .docker/nginx/entrypoint.sh
+++ .docker/nginx/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-dockerize -wait tcp://app:9000 -timeout 100s \
+dockerize -wait tcp://app:9000 -timeout 300s \
     -template ./.docker/nginx/nginx.tmpl:/etc/nginx/conf.d/default.conf
 
 nginx -g 'daemon off;'
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ composer install
 
 chown -R www-data:www-data storage
 
-# Try to connect to the database every 1 second over 50 seconds
-dockerize -wait tcp://db:3306 -timeout 50s
+# Try to connect to the database every 1 second over 120 seconds
+dockerize -wait tcp://db:3306 -timeout 120s
 
 php artisan cache:clear
 php artisan config:clear
