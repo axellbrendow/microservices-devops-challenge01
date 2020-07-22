2bc62f0720831bbcca1bf154ee263a6ef2a7ab2b
axell-brendow <breno.axel@gmail.com>
2020-07-22 19:45:19 -0300

    Nginx does not need to wait php application

--- .docker/nginx/entrypoint.sh
+++ .docker/nginx/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 
-dockerize -wait tcp://app:9000 -timeout 300s \
-    -template ./.docker/nginx/nginx.tmpl:/etc/nginx/conf.d/default.conf
+dockerize -template ./.docker/nginx/nginx.tmpl:/etc/nginx/conf.d/default.conf
 
 nginx -g 'daemon off;'
