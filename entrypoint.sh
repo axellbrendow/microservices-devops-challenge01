8e355ed8213bf7d291cc8addd923d13600d229ad
axellbrendow <axellbrendow@ioasys.com.br>
2021-01-10 09:02:23 -0300

    Update mysql service name

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ composer install
 chown -R www-data:www-data storage
 
 # Try to connect to the database every 1 second over 120 seconds
-dockerize -wait tcp://db:3306 -timeout 120s
+dockerize -wait tcp://mysql-service:3306 -timeout 120s
 
 php artisan cache:clear
 php artisan config:clear
